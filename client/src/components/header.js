bf743c70780c40eb4af6010d8d7a05d7a5e09efc
Adam Klepacz <adamklepacz92@gmail.com>
2022-05-05 15:02:09 +0200

    update colors

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { colors, widths } from '../styles';
-import styled from '@emotion/styled';
-import { Link } from '@reach/router';
-import logo from '../assets/space_cat_logo.png';
+import React from "react";
+import { colors, widths } from "../styles";
+import styled from "@emotion/styled";
+import { Link } from "@reach/router";
+import logo from "../assets/spacedog.png";
 
 /**
  * Header renders the top navigation
@@ -19,8 +19,8 @@ const Header = ({ children }) => {
                 <Logo src={logo} />
               </LogoContainer>
               <Title>
-                <h3>Catstronaut</h3>
-                <div>Kitty space academy</div>
+                <h3>Dogeronaut</h3>
+                <div>Dog space academy</div>
               </Title>
             </HomeButton>
           </HomeLink>
@@ -35,15 +35,15 @@ export default Header;
 
 /** Header styled components */
 const HeaderBar = styled.div({
-  display: 'flex',
-  flexDirection: 'row',
-  alignItems: 'center',
-  justifyContent: 'center',
-  borderBottom: `solid 1px ${colors.pink.light}`,
-  boxShadow: '0px 1px 5px 0px rgba(0,0,0,0.15)',
-  padding: '5px 30px',
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+  justifyContent: "center",
+  borderBottom: `solid 1px ${colors.indigo.base}`,
+  boxShadow: "0px 1px 5px 0px rgba(0,0,0,0.15)",
+  padding: "5px 30px",
   minHeight: 80,
-  backgroundColor: 'white',
+  backgroundColor: "white",
 });
 
 const Container = styled.div({
@@ -51,25 +51,25 @@ const Container = styled.div({
 });
 
 const HomeLink = styled(Link)({
-  textDecoration: 'none',
+  textDecoration: "none",
 });
 
 const HomeButtonContainer = styled.div({
-  display: 'flex',
+  display: "flex",
   flex: 1,
 });
 
 const HomeButton = styled.div({
-  display: 'flex',
-  flexDirection: 'row',
+  display: "flex",
+  flexDirection: "row",
   color: colors.accent,
-  alignItems: 'center',
-  ':hover': {
-    color: colors.pink.dark,
+  alignItems: "center",
+  ":hover": {
+    color: colors.indigo.dark,
   },
 });
 
-const LogoContainer = styled.div({ display: 'flex', alignSelf: 'center' });
+const LogoContainer = styled.div({ display: "flex", alignSelf: "center" });
 
 const Logo = styled.img({
   height: 60,
@@ -78,15 +78,15 @@ const Logo = styled.img({
 });
 
 const Title = styled.div({
-  display: 'flex',
-  flexDirection: 'column',
+  display: "flex",
+  flexDirection: "column",
   h3: {
-    lineHeight: '1em',
+    lineHeight: "1em",
     marginBottom: 0,
   },
   div: {
-    fontSize: '0.9em',
-    lineHeight: '0.8em',
+    fontSize: "0.9em",
+    lineHeight: "0.8em",
     paddingLeft: 2,
   },
 });
